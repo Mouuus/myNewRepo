7dbe847cd2e71acda6605f615699fb07b562175b
Mouuus <Muhammd.ramadan@gmail.com>
2021-11-11 02:06:09 +0200

    modified commit2

--- Back-End/app.js
+++ Back-End/app.js
@@ -8,7 +8,8 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
-//   this is a new line
+//   this is a new line 
+// and here comes a new line again for branch
   app.use(cors(corsOptions));
 
 app.get('/',(req,res)=>{
